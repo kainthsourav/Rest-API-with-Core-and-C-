f8a54a3fb8874d42a428054671b7ad2620ad87da
kainthsourav <souravkainth@outlook.com>
2020-10-01 19:56:59 +0530

    First Web API

---
+++ FirstWebApiWithCore/FirstWebApiWithCore/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FirstWebApiWithCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        static List<Products> _products = new List<Products>()
+       {
+           new Products(){productID=10,productName="Laptop",productPrice="20000"},
+           new Products(){productID=20,productName="Phone",productPrice="1500"}
+       };
+
+        [HttpGet]
+        public IEnumerable<Products> Get() => _products;
+
+
+        [HttpPost]
+        public void Post([FromBody] Products products) => _products.Add(products);
+
+        [HttpPut("{productID}")]
+        public void Put(int productID, [FromBody] Products products) => _products[productID] = products;
+
+        [HttpDelete("{productID}")]
+        public void Delete(int productID) => _products.RemoveAt(productID);
+   
+   }
+}
+    
---
+++ FirstWebApiWithCore/FirstWebApiWithCore/Models/Products.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FirstWebApiWithCore
+{
+    public class Products
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public string productPrice { get; set; }
+
+    }
+}
