18331b70674ac5adcbcf047701b29fb3457796cf
kainthsourav <souravkainth@outlook.com>
2020-10-01 21:08:08 +0530

    Data Annotation Validation in Model

---
+++ FirstWebApiWithCore/FirstWebApiWithCore/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstWebApiWithCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApiWithCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        static List<Customers> _customers = new List<Customers>()
+        {
+            new Customers(){customerID=10,customerName="Sourav",customerEmail="sourav@xml.com",customePhone="+91-8427-XXXX-XX"},
+             new Customers(){customerID=20,customerName="Sourav",customerEmail="souravkai@xml.com",customePhone="+91-8426-XXXX-XX"}
+        };
+
+        public IActionResult Get()
+        {
+            return Ok(_customers);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Customers customers)
+        {
+            if (ModelState.IsValid) //To check validtion we gave in Model 
+            {
+                _customers.Add(customers);
+                return StatusCode(201);
+            }
+            else
+                return BadRequest(ModelState);
+            
+        }
+    }
+}
+  
---
+++ FirstWebApiWithCore/FirstWebApiWithCore/Models/Customers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebApiWithCore.Models
+{
+    public class Customers
+    {
+        [Required]
+        public int customerID { get; set; }
+        [Required,StringLength(15)]
+        public string customerName { get; set; }
+        [RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$",ErrorMessage ="Email Format Not Valid")]
+        public string customerEmail { get; set; }
+        public string customePhone { get; set; }
+
+    }
+}
